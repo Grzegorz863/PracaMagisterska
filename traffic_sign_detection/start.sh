6e7a4b3845b7b6552a62d2c83889efdccc0e5f81
Grzegorz863 <gn22@o2.pl>
2020-07-22 23:30:47 +0200

    dodano plik konfigurujący środowisko w Ziemowicie

---
+++ traffic_sign_detection/start.sh
@@ -0,0 +1,15 @@
+conda deactivate
+conda activate detection2
+cd /home/gnowak/do_pracy_mrg/model-11/research
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+cd
+cd detection
+
+
+conda deactivate
+conda activate eval
+cd /home/gnowak/do_pracy_mrg/model-11/research
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+cd
+cd detection
+
